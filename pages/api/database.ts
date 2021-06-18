3fd1ae8db3a22fa541fefeccb21e8f5da7f483be
callumok2004 <callumokane123@gmail.com>
2021-06-18 15:57:41 +0100

    LOVELY

--- pages/api/database.ts
+++ pages/api/database.ts
@@ -6,7 +6,7 @@ const client = new MongoClient(process.env.MONGO_URI as string, {
 });
 
 export default async function database(dbName: string, collName: string) {
-    if (typeof dbName !== "string") return (collName as any).json({ msg: "no" });
+    if (typeof dbName !== "string") return (collName as unknown as { json(inp: object) }).json({ msg: "no" });
     if (!client.isConnected()) await client.connect();
     return client.db(dbName).collection(collName);
 }
